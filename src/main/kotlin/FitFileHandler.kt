890b62e6c31e628348e77e62acf44db05585368c
Vincent Martin <vince@finalatomicbuster.net>
2022-12-27 11:59:32 -0800

    Add tests for /fit/{path} param

--- src/main/kotlin/FitFileHandler.kt
+++ src/main/kotlin/FitFileHandler.kt
@@ -7,10 +7,13 @@ object FitFileHandler {
     fun getFitFile(ctx: Context) {
         val bhClient = BikeHopperClient()
         // Use bhClient to fetch our routes after pulling out the "path" param which is only used locally for us.
-        val routeData = bhClient.fetchRoute(ctx.queryParamMap().filter{ it.key != "path"})
+        val routeData = bhClient.fetchRoute(ctx.queryParamMap().filter { it.key != "path" })
 
         // Quick check that the path we choose actually exists and has just a single leg.
-        val path = ctx.queryParamAsClass<Int>("path", Int::class.java).check({it >= 0 && it < routeData.paths.size && routeData.paths[it].legs.size == 1 }, "Invalid path requested").get()
+        val path = ctx.queryParamAsClass<Int>("path", Int::class.java).check(
+            { it >= 0 && it < routeData.paths.size && routeData.paths[it].legs.size == 1 },
+            "Invalid path selection, must be between 0 and ${routeData.paths.size}"
+        ).get()
         val bikeHopperFileCreator = BikeHopperFileCreator(routeData, path)
 
         ctx.contentType("application/vnd.ant.fit")
