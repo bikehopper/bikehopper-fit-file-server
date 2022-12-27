fa43733217941bd22b46dc16b862347d0a549549
Vincent Martin <vince@finalatomicbuster.net>
2022-12-27 10:09:13 -0800

    Add implementation testing for /fit/

--- src/test/kotlin/FunctionalTests.kt
+++ src/test/kotlin/FunctionalTests.kt
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -11,14 +13,15 @@ import io.ktor.client.plugins.logging.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.assertEquals
 import kotlinx.coroutines.runBlocking
+import com.garmin.fit.*
 
 @DisplayName("Functional Tests for Fit-Server")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class FunctionalTests {
     private val fitServer = FitServer()
     private val client = HttpClient(CIO)
+    private val port = 9001
 
     @BeforeAll
     fun beforeTests() {
@@ -34,7 +37,39 @@ class FunctionalTests {
     @Test
     fun checkHealth() {
         assertEquals("OK", runBlocking {
-            client.get("http://localhost:9001/health").body()
+            client.get("http://localhost:${port}/health").bodyAsText()
         } )
+    }
+
+    @DisplayName("Test GET - /fit/, FIT data created is valid Fit Data")
+    @Test
+    fun checkFitFile() {
+        val params = mapOf(
+            "locale" to listOf("en-US"),
+            "elevation" to listOf("true"),
+            "useMiles" to listOf("false"),
+            "layer" to listOf("OpenStreetMap"),
+            "profile" to listOf("pt"),
+            "optimize" to listOf("true"),
+            "pointsEncoded" to listOf("false"),
+            "pt.earliest_departure_time" to listOf("2022-11-01T02:31:44.439Z"),
+            "pt.connecting_profile" to listOf("bike2"),
+            "pt.arrive_by" to listOf("false"),
+            "details" to listOf("cycleway", "road_class", "street_name"),
+            "point" to listOf("37.78306,-122.45867", "37.78516,-122.46238"),
+            "path" to listOf("0")
+        )
+
+        // Get fit byte array.
+        var fitData: ByteArray = runBlocking {
+            client.get("http://localhost:${port}/fit"){
+                url {
+                    params.forEach { (key, values) -> values.forEach { value -> parameters.append(key, value)}}
+                }
+            }.body()
+        }
+
+        val decoder = Decode()
+        assertTrue(decoder.checkFileIntegrity(fitData.inputStream()), "Data is not in Fit format")
     }
 }
