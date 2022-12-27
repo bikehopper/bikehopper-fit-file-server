305e5966cabab8b447c62b098be331d93c09f7e4
Vincent Martin <vince@finalatomicbuster.net>
2022-12-27 10:17:10 -0800

    Update jUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation("io.ktor:ktor-client-logging:2.1.3")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
     implementation("io.ktor:ktor-client-content-negotiation:2.1.3")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.test {
